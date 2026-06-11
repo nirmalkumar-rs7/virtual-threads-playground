6e12a78fca59d8b2cb2b6bf9c8cc3dc25b3ecfcc
nirmalkumar.rs <nirmalkumar.rs@zohocorp.com>
2026-06-11 16:00:07 +0530

    Add RaceConditionDemo and SynchronizationDemo for thread synchronization examples

---
+++ src/main/java/org/storm/synchronization/SynchronizationDemo.java
@@ -0,0 +1,39 @@
+package org.storm.synchronization;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.storm.CommonUtil;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SynchronizationDemo {
+
+    private static final Logger logger = LoggerFactory.getLogger(SynchronizationDemo.class);
+    private static List<Long> list = new ArrayList<>();
+
+    static void main() {
+//        demo(Thread.ofPlatform());
+        demo(Thread.ofVirtual());
+        CommonUtil.sleep(Duration.ofSeconds(2));
+        logger.info("list size: {}", list.size());
+    }
+
+    private static void demo(Thread.Builder builder) {
+        for (int i = 0; i < 50; i++) {
+            builder.start(() -> {
+                logger.info("Task Strated {}", Thread.currentThread());
+                for (int j = 0; j < 200; j++) {
+                    inMemoryTask();
+                }
+                logger.info("Task Completed {}", Thread.currentThread());
+            });
+        }
+    }
+
+    private static synchronized void inMemoryTask() {
+        list.add(1L);
+    }
+
+}
